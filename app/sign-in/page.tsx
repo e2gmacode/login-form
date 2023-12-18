bc9dc1c2d57a647afe3cefc2ce35a87b5c048a81
e2gmacode <e2gamcode@gmail.com>
2023-12-19 00:41:41 +0900

    ログイン画面のログイン処理を追加

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import { useState, MouseEvent } from 'react';
+import { useState, MouseEvent, useEffect } from 'react';
 import Link from 'next/link';
+import { User, onAuthStateChanged, signInWithEmailAndPassword } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 import styles from './page.module.css';
+import { auth } from '../firebase/config';
 
 /**
- * サインプアップ画面
+ * ログイン画面
  *
  * @param void
  * @returns JSX.Element
@@ -15,9 +18,15 @@ const SignIn = () => {
   const [email, setEmail] = useState('');
   // パスワード
   const [password, setPassword] = useState('');
+  // ログインユーザー
+  const [user, setUser] = useState<User | null>();
+
+  // ルーター
+  const router = useRouter();
 
   // エラーメッセージ
   const [message, setMessage] = useState<string[]>();
+  const [loginMessage, setLoginMessage] = useState<string>('');
 
   // バリデート
   const validate = () => {
@@ -58,50 +67,76 @@ const SignIn = () => {
 
     // バリデート
     if (!validate()) {
-      // バリデートチェックを通過したら、認証情報登録を行う
+      // バリデートチェックを通過したら、ログイン処理を行う
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+      } catch (e) {
+        setLoginMessage('メールアドレスまたはパスワードが違います');
+      }
     }
   };
 
+  // レンダリング時に実施
+  useEffect(() => {
+    // ログイン状態をチェック
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  });
+
   return (
-    <main className={styles.main}>
-      <form className={styles.form}>
-        <h3>ログイン画面</h3>
+    <>
+      {user ? (
+        router.push('/')
+      ) : (
+        <main className={styles.main}>
+          <form className={styles.form}>
+            <h3>ログイン画面</h3>
 
-        <label htmlFor="email">メールアドレス</label>
-        <input
-          type="email"
-          value={email}
-          placeholder="メールアドレス"
-          id="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        {message !== undefined && message[0] !== '' ? (
-          <span className={styles.err_message}>{message[0]}</span>
-        ) : (
-          ''
-        )}
+            <label htmlFor="email">メールアドレス</label>
+            <input
+              type="email"
+              value={email}
+              placeholder="メールアドレス"
+              id="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {message !== undefined && message[0] !== '' ? (
+              <span className={styles.err_message}>{message[0]}</span>
+            ) : (
+              ''
+            )}
 
-        <label htmlFor="password">パスワード</label>
-        <input
-          type="password"
-          value={password}
-          placeholder="パスワード"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {message !== undefined && message[1] !== '' ? (
-          <span className={styles.err_message}>{message[1]}</span>
-        ) : (
-          ''
-        )}
+            <label htmlFor="password">パスワード</label>
+            <input
+              type="password"
+              value={password}
+              placeholder="パスワード"
+              id="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {message !== undefined && message[1] !== '' ? (
+              <span className={styles.err_message}>{message[1]}</span>
+            ) : (
+              ''
+            )}
 
-        <Link href="sign-up">※新規登録はこちら</Link>
+            <Link href="sign-up">※新規登録はこちら</Link>
 
-        <button type="button" onClick={handleLogin}>
-          ログイン
-        </button>
-      </form>
-    </main>
+            {/* ログインエラーメッセージ */}
+            {loginMessage !== '' ? (
+              <span className={styles.login_err_message}>{loginMessage}</span>
+            ) : (
+              ''
+            )}
+
+            <button type="button" onClick={handleLogin}>
+              ログイン
+            </button>
+          </form>
+        </main>
+      )}
+    </>
   );
 };
 
