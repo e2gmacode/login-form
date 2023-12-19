f90251fb03d010cb72818c1ca75bd2de2c9faaf1
松井一人 <matsui@kmkworld.com>
2023-12-19 19:44:00 +0900

    テストモジュールの追加、Jestの設定

---
+++ app/__test__/SignUp.test.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react';
+import { render, screen } from '@testing-library/react';
+import SignUp from '../sign-up/page';
+
+const mockedUseRouter = jest.fn();
+
+jest.mock('next/navigation', () => ({
+  useRouter: () => mockedUseRouter(),
+  usePathname: jest.fn().mockReturnValue('/sign-in'),
+}));
+
+describe('サインアップ画面のテスト', () => {
+  it('表示項目確認（サインアップ画面）', async () => {
+    render(<SignUp />);
+    expect(screen.getByText('サインアップ画面')).toBeInTheDocument();
+  });
+});
